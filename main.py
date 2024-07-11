e07308f2f2fe8515b2387bc48cc88c846412afc5
Андрей Шеремет <seheremet.and@yandex.ru>
2024-07-11 17:46:10 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 # Изменяемые и неизменяемые объекты. Кортежи 1.5
-immutable_var = ([1, 2, 'a', 'b'])
-print(immutable_var)
+immutable_var = 1, 2, 'a', 'b'
 # immutable_var[1] = v невозможно так как элемент в кортеже неизменяем
+print(immutable_var)
 mutable_list = [1, 2, 'a', 'b']
 mutable_list[3] = 'v'
 print(mutable_list)
